b9a418b0aae0335cafee44086c3ef6c3f529f454
slayingMoon <g.kolev15@gmail.com>
2022-09-26 13:46:47 +0300

    added second solution to problem 3.

---
+++ 03_Objects_Composition/city-taxes-2.js
@@ -0,0 +1,28 @@
+function createRecord(name, population, treasury) {
+
+        //return a nameless object, holding the properties
+        return {
+            name,
+            population,
+            treasury,
+            taxRate: 10,
+            collectTaxes() {
+                this.treasury += this.population * this.taxRate;
+            },
+            applyGrowth(percentage) {
+                this.population += Math.floor(this.population * percentage / 100);
+            },
+            applyRecession(percentage) {
+                this.treasury -= Math.floor(this.treasury * percentage / 100);
+            }
+        };
+}
+
+const city =
+    createRecord('Tortuga',
+        7000,
+        15000);
+city.collectTaxes();
+console.log(city.treasury);
+city.applyGrowth(5);
+console.log(city.population);
