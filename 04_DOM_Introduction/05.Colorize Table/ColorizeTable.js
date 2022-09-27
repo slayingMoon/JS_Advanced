90ae016ea2c5e31076be47ea13e5bd38ec3d64ef
slayingMoon <g.kolev15@gmail.com>
2022-09-27 19:12:44 +0300

    added solution to problem 5

---
+++ 04_DOM_Introduction/05.Colorize Table/ColorizeTable.js
@@ -0,0 +1,9 @@
+function colorize() {
+    let rowElements = document.querySelectorAll('table tr');
+
+    for(let i = 0; i < rowElements.length; i++) {
+        if (i % 2 !== 0) {
+            rowElements[i].style.backgroundColor = 'teal';
+        }
+    }
+}
