3b90c0bd224bb3c8d0539c36d78e864e70b22e0c
slayingMoon <g.kolev15@gmail.com>
2022-09-28 09:01:37 +0300

    deleted old folder

--- 04_DOM_Introduction/02.Collect List Items/CollectListItems.js
+++
@@ -1,6 +0,0 @@
-function extractText() {
-    let ulElement = document.getElementById('items');
-
-    let textareaElement = document.getElementById('result');
-    textareaElement.textContent = ulElement.textContent;
-}
--- 04_DOM_Introduction/05.Colorize Table/ColorizeTable.js
+++
@@ -1,9 +0,0 @@
-function colorize() {
-    let rowElements = document.querySelectorAll('table tr');
-
-    for(let i = 0; i < rowElements.length; i++) {
-        if (i % 2 !== 0) {
-            rowElements[i].style.backgroundColor = 'teal';
-        }
-    }
-}
