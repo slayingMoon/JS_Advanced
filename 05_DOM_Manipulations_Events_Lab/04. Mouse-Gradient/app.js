5470fa2b1db0a149e13f360938801fa11b2f10dc
slayingMoon <g.kolev15@gmail.com>
2022-09-30 14:51:23 +0300

    added solution to problem 4

--- 05_DOM_Manipulations_Events_Lab/04. Mouse-Gradient/app.js
+++ 05_DOM_Manipulations_Events_Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,20 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientElement = document.getElementById('gradient');
+    let resultElement = document.getElementById('result');
+
+    function gradientMouseoverHandler(event) {
+        let position = event.offsetX;
+        let gradientWidth = event.target.clientWidth - 1;
+
+        let percent = Math.floor((position / gradientWidth) * 100);
+
+        resultElement.textContent = `${percent}%`;
+    }
+
+    function gradientOut(event) {
+        resultElement.textContent = '';
+    }
+
+    gradientElement.addEventListener('mousemove', gradientMouseoverHandler);
+    gradientElement.addEventListener('mouseout', gradientOut);
 }
