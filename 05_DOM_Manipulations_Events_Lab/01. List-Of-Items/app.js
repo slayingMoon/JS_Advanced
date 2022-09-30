0e0231ae6ac6e32f1238ffe42b0907d2ab4c3519
slayingMoon <g.kolev15@gmail.com>
2022-09-30 10:34:15 +0300

    added solutions to problems 1 and 2

--- 05_DOM_Manipulations_Events_Lab/01. List-Of-Items/app.js
+++ 05_DOM_Manipulations_Events_Lab/01. List-Of-Items/app.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+    //it is important to only get the input element without it's text value
+    //so that we can later use the already found element call it's value and assign an empty string to it
+    let inputElememt = document.getElementById('newItemText');
+    let itemsList = document.getElementById('items');
+
+    //creating the new item and assigning value to it
+    let liElement = document.createElement('li');
+    liElement.textContent = inputText.value;
+
+    //adding the new item to the list
+    itemsList.appendChild(liElement);
+
+    //clearing input
+    inputText.value = '';
 }
--- 05_DOM_Manipulations_Events_Lab/02. Delete-From-Table/app.js
+++ 05_DOM_Manipulations_Events_Lab/02. Delete-From-Table/app.js
@@ -1,3 +1,24 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    //.value is used only in cases where we have input from user
+    let emailInput = document.getElementsByName('email')[0].value.trim(); //to remove unwanted white space in input
+    //gets all cells containing email value in the table - td:nth-child(2)
+    //because the email is the second td child element 
+    let emailElements = document.querySelectorAll('tbody tr td:nth-child(2)');
+    let resultElement = document.getElementById('result');
+
+    let emailElementsArray = Array.from(emailElements);
+    //targetEmailElement is <td>emailValue</td>
+    let targetEmailElement = emailElementsArray.find(td => td.textContent == emailInput);
+    
+    //<td>emailValue</td>
+    if(targetEmailElement) {
+        //the parent element is <tr></tr> -> we need to delete the whole row,
+        //containing the given email
+        targetEmailElement.parentElement.remove();
+
+        resultElement.textContent = 'Deleted.';
+    }else {
+        resultElement.textContent = 'Not found.';
+    }
+
 }
