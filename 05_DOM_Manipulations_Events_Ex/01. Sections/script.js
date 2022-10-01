e7379cf66809d398a5cdd3221f25fd1eb52dd1a9
slayingMoon <g.kolev15@gmail.com>
2022-10-01 09:57:25 +0200

    added solution to problem 1

--- 05_DOM_Manipulations_Events_Ex/01. Sections/script.js
+++ 05_DOM_Manipulations_Events_Ex/01. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+
+   for(let word of words) {
+      let newDiv = document.createElement('div');
+      let p = document.createElement('p');
+      p.innerText = word;
+      p.style.display = 'none';
+      newDiv.appendChild(p);
+      newDiv.addEventListener('click', showP);
+      document.getElementById('content').appendChild(newDiv);
+   }
+
+   function showP(event) {
+      // event.target.firstChild.style.display = 'block'; -- another option to get child
+      event.target.children[0].style.display = 'block';
+   }
 }
