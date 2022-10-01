00d0372d3280b3f150b8982327970faf0abc6398
slayingMoon <g.kolev15@gmail.com>
2022-10-01 13:51:37 +0200

    added solution to problem 3

--- 05_DOM_Manipulations_Events_Ex/03. Locked-Profile/app.js
+++ 05_DOM_Manipulations_Events_Ex/03. Locked-Profile/app.js
@@ -1,3 +1,37 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let buttons = Array.from(document.getElementsByTagName('button'));
+
+    buttons.forEach(b => b.addEventListener('click', showMore));
+
+    //Lock/Unlock
+    let radioButtons = Array.from(document.querySelectorAll('input[type=radio]'));
+    radioButtons.forEach(rb => rb.addEventListener('change', changeStatus));
+
+    function showMore(event) {
+        let button = event.target;
+        let profileElement = button.parentElement;
+        let rbuttons = profileElement.querySelectorAll('input[type=radio]');
+
+        if (rbuttons[0].checked) {
+            return;
+        }
+
+        let userDetailsElement = profileElement.children[9];
+
+        if(button.textContent == 'Show more') {
+            userDetailsElement.style.display = 'block';
+            button.textContent = 'Hide it';
+        }else {
+            userDetailsElement.style.display = 'none';
+            button.textContent = 'Show more';
+        }
+
+        debugger
+    }
+
+    function changeStatus(event) {
+        let currentButton = event.target;
+        currentButton.checked = true;
+        debugger
+    }
 }
