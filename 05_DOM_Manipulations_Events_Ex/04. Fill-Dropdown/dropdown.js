418bb1de1874bc7d79a69ae1e0a9774d529750bd
slayingMoon <g.kolev15@gmail.com>
2022-10-02 07:16:46 +0200

    added solution to problem 4

--- 05_DOM_Manipulations_Events_Ex/04. Fill-Dropdown/dropdown.js
+++ 05_DOM_Manipulations_Events_Ex/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    let inputElements = Array.from(document.querySelectorAll('input[type=text]'));
+    let text = inputElements[0];
+    let value = inputElements[1];
+
+    let newOptionElement = document.createElement('option');
+    newOptionElement.value = value.value;
+    newOptionElement.textContent = text.value;
+
+    let dropdownElement = document.getElementById('menu');
+    dropdownElement.appendChild(newOptionElement);
+    
+    text.value = '';
+    value.value = '';
 }
