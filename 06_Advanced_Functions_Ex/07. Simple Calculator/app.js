c7cdedcda97c6c3ecedaef0ffec0f9224bf9dbf6
slayingMoon <g.kolev15@gmail.com>
2022-10-04 17:18:01 +0300

    added solution to problem 7

--- 06_Advanced_Functions_Ex/07. Simple Calculator/app.js
+++ 06_Advanced_Functions_Ex/07. Simple Calculator/app.js
@@ -1,7 +1,42 @@
 function calculator() {
-    // TODO:
+
+    const html = {
+        s1: '',
+        s2: '',
+        result: ''
+    }
+
+    function calc(a, b, sign) {
+        const signs = {
+            '+': (a,b) => a + b,
+            '-': (a,b) => a - b
+        };
+
+        return signs[sign](Number(a), Number(b));
+    }
+    
+    //Your function should return an object that meets the specified requirements
+    //The returned object should support the following functionality
+    //•	init (selector1, selector2, resultSelector) 
+    //•	add () 
+    //•	subtract () 
+    return {
+        init: (selector1, selector2, resultSelector) => {
+            html.s1 = document.querySelector(selector1);
+            html.s2 = document.querySelector(selector2);
+            html.result = document.querySelector(resultSelector);
+        },
+        add: () => {
+            html.result.value = calc(html.s1.value, html.s2.value, '+');
+        },
+        subtract: () => {
+            html.result.value = calc(html.s1.value, html.s2.value, '-');
+        }
+    }
 }
 
+const calculate = calculator (); 
+calculate.init ('#num1', '#num2', '#result');
 
 
 
