8e1c0c723f8aba8d2c335aad99e0e7cce7129497
slayingMoon <g.kolev15@gmail.com>
2022-10-06 08:28:02 +0300

    added solution to problem 6

--- 07_UnitTesting_ErrorHandling_Lab/RGBtoHex/rgb-to-hex.test.js
+++ 07_UnitTesting_ErrorHandling_Lab/RGBtoHex/rgb-to-hex.test.js
@@ -1,7 +1,7 @@
 const { expect } = require('chai')
 const { rgbToHexColor } = require('./rgb-to-hex')
 
-describe('Testing RGB to Hex function', () => {
+describe('Testing RGB integers Color to Hexadecimal functionality', () => {
 
     /**
      * Take three integer numbers, representing the red, green, and blue values of RGB color, each within the range [0…255]
@@ -9,19 +9,44 @@ describe('Testing RGB to Hex function', () => {
      * Return undefined if any of the input parameters are of an invalid type or not in the expected range
      */
 
-    it('should return undefined if no arguments are passed', () => {
+    it('should return #ADFF32 if arguments are (173, 255, 50)', () => {
         //Arrange
+        let red = 173;
+        let green = 255;
+        let blue = 50;
         //Act
-        let result = rgbToHexColor()
+        let result = rgbToHexColor(red, green, blue)
+        //Assert
+        expect(result).to.be.equal('#ADFF32');
+    })
+
+    it('should return undefined if invalid range red', () => {
+        //Arrange
+        let red = -1;
+        let green = 0;
+        let blue = 0;
+        //Act
+        let result = rgbToHexColor(red, green, blue)
         //Assert
         expect(result).to.be.undefined
     })
 
-    it('should return undefined if arguments are not valid', () => {
+    it('should return undefined if invalid range green', () => {
         //Arrange
-        let red = 'a';
-        let green = 'b';
-        let blue = 'c';
+        let red = 0;
+        let green = -1;
+        let blue = 0;
+        //Act
+        let result = rgbToHexColor(red, green, blue)
+        //Assert
+        expect(result).to.be.undefined
+    })
+
+    it('should return undefined if invalid range blue', () => {
+        //Arrange
+        let red = 0;
+        let green = 0;
+        let blue = -1;
         //Act
         let result = rgbToHexColor(red, green, blue)
         //Assert
@@ -37,6 +62,28 @@ describe('Testing RGB to Hex function', () => {
         let result = rgbToHexColor(red, green, blue)
         //Assert
         expect(result).to.be.equal('#010203');
+    })
+
+    it('should return #000000 if arguments are 0, 0 ,0', () => {
+        //Arrange
+        let red = 0;
+        let green = 0;
+        let blue = 0;
+        //Act
+        let result = rgbToHexColor(red, green, blue)
+        //Assert
+        expect(result).to.be.equal('#000000');
+    })
+
+    it('should return undefined if invalid range above 255 - (173, 256, 50)', () => {
+        //Arrange
+        let red = 173;
+        let green = 256;
+        let blue = 50;
+        //Act
+        let result = rgbToHexColor(red, green, blue)
+        //Assert
+        expect(result).to.be.undefined;
     })
 
 })
