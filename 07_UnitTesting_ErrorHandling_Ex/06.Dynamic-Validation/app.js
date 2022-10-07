b333e2e0277b389b5f187d152f9adb88ebb58568
slayingMoon <g.kolev15@gmail.com>
2022-10-07 09:07:32 +0300

    added solution to problem 6

--- 07_UnitTesting_ErrorHandling_Ex/06.Dynamic-Validation/app.js
+++ 07_UnitTesting_ErrorHandling_Ex/06.Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    // TODO
+    let inputField = document.getElementById('email');
+    let emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/g;
+
+    inputField.addEventListener('change', e => applyStyle(e.target, e.target.value));
+
+    function applyStyle(e, email) {
+        //we have a css style for class='error' <style>.error{}</style>
+        e.className = isEmailValid(email) ? '' : 'error';
+    }
+
+    function isEmailValid(input) {
+        return emailPattern.test(input);
+    }
 }
